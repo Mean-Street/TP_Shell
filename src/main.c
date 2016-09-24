1fec3937a49564691ce4e5d77b887e3eb2298a58
PiggyGenius <ludo.itrts@gmail.com>
2016-09-24 16:21:19 +0200

    ...

--- src/main.c
+++ src/main.c
@@ -19,7 +19,7 @@ int main(int argc,char** argv){
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_RESTART;
 	sa.sa_handler = childhandler;
-	//sigaction(SIGCHLD,&sa,NULL);
+	sigaction(SIGCHLD,&sa,NULL);
 	pid_t pid = 1;
 
 	for(uint8_t i=0;i<10;i++){
@@ -28,7 +28,7 @@ int main(int argc,char** argv){
 			execl("/bin/ls","ls",NULL);
 		}
 		else {
-			waitpid(pid,NULL,WNOHANG);
+			//waitpid(pid,NULL,WNOHANG);
 			;
 		}
 	}
