705101c32f30ba370fa671dbb53ce9e6466e6860
piggygenius <ludo.itrts@gmail.com>
2016-10-16 14:03:50 +0200

    Fixing ensishell, remove continue and add setup_line

--- src/ensishell.c
+++ src/ensishell.c
@@ -84,7 +84,6 @@ int main()
 	sigaction(SIGCHLD,&sa,NULL);
 
 	char *line = NULL;
-	int res;
 	char *prompt = "ensishell>";
 	struct cmdline *l;
 
@@ -103,8 +102,6 @@ int main()
 #if USE_GNU_READLINE == 1
 		add_history(line);
 #endif
-
-
 #if USE_GUILE == 1
 		/* The line is a scheme command */
 		if (line[0] == '(') {
@@ -112,10 +109,7 @@ int main()
 			continue;
 		}
 #endif
-
-		/* parsecmd free line and set it up to 0 */
-
-			continue;
+		setup_line(&l,line,jobs_list);
 		if (l->seq[0] != NULL) {
 			create_process(jobs_list, l);
 		}
